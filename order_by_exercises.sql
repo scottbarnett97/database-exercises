c138afce0ab49802d21ca55325eb81284ddf541a
Scott Barnett <scottbarnett97@gmail.com>
2023-03-09 15:06:18 -0600

    updated where exercise and added order exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,65 @@
+show databases;
+use employees;
+select database ();
+show tables;
+-- 2. Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+select *
+from employees
+ where first_name = 'Irena'or first_name = 'Vidya' or first_name = 'Maya'
+    order by first_name;
+    -- Irena Reuten
+
+-- 3. Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+select *
+from employees
+ where first_name = 'Irena'or first_name = 'Vidya' or first_name = 'Maya'
+    order by first_name, last_name;
+    -- Irena Acton
+
+-- 4. Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
+select *
+from employees
+ where first_name = 'Irena'or first_name = 'Vidya' or first_name = 'Maya'
+    order by last_name, first_name;   
+    -- Irena Acton, Maya Zyda
+
+-- 5. Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their employee number. Enter a comment with the number of employees returned, the first employee number and their first and last name, and the last employee number with their first and last name.
+select *
+from employees
+ where last_name like "e%" and last_name like "%e"
+    order by emp_no;
+select count(*)
+from employees
+ where last_name like "e%" and last_name like "%e"
+    order by emp_no;    
+    
+    -- count 899, 10021 Ramzi Erde, 499648 Tadahiro Erde
+    
+-- 6.  Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their hire date, so that the newest employees are listed first. Enter a comment with the number of employees returned, the name of the newest employee, and the name of the oldest employee.
+select *
+from employees
+ where last_name like "e%" and last_name like "%e"
+    order by hire_date DESC; 
+select count(*)
+from employees
+ where last_name like "e%" and last_name like "%e"
+    order by hire_date DESC; 
+    -- 899 count, newest employee Teiji Eldridge, oldest employee Sergi Erde
+    
+-- 7. Find all employees hired in the 90s and born on Christmas. Sort the results so that the oldest employee who was hired last is the first result. Enter a comment with the number of employees returned, the name of the oldest employee who was hired last, and the name of the youngest employee who was hired first.
+select *
+from employees
+where hire_date like "199%"
+	and birth_date like "%-12-25"
+    order by birth_date ASC, hire_date DESC;
+    select count(*)
+from employees
+where hire_date like "199%"
+	and birth_date like "%-12-25"
+    order by birth_date ASC, hire_date DESC;
+    
+    -- count 362, Khun Bernini, Douadi Pettis
+
+--    
+    
+    -- 
--- where_advanced_exercises.sql
+++ where_advanced_exercises.sql
@@ -3,28 +3,84 @@ use employees;
 select database ();
 show tables;
 -- 1. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. What is the employee number of the top three results?
-/*
+
 select *
 from employees
 where first_name IN (	'Irena', 'Vidya', 'Maya')
 ;
-*/
--- 499750, 499588, 497788
+
+-- 10200, 10397, 10610
 
 -- 2. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. What is the employee number of the top three results? Does it match the previous question?
-/*
+
 select *
-from employees, dept_emp
-where first_name = 'Irena'or 'Vidya' or 'Maya'
+from employees
+where first_name = 'Irena'or first_name = 'Vidya' or first_name = 'Maya'
 ;
-*/
--- 494902, 494802, 492914 they do not match because it is grouping the names
+
+-- 10200, 10397, 10610
 
 -- 3. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. What is the employee number of the top three results.
 
 select *
-from employees, dept_emp
-where first_name = 'Irena' or 'Vidya' or 'Maya'
-	and gender = 'M'
-;
+from employees
+ where first_name = 'Irena'or first_name = 'Vidya' or first_name = 'Maya'
+	and gender = "M";
+-- 10200, 10397, 10610
+
+-- 4. Find all unique last names that start with 'E'.
+
+select distinct last_name
+from employees
+where last_name like "e%";
+
+-- 5. Find all unique last names that start or end with 'E'.
+
+select distinct last_name
+from employees
+where last_name like "e%" 
+or last_name like "%e";
+
+-- 6. Find all unique last names that end with E, but does not start with E?
+select distinct last_name
+from employees
+where last_name not like "e%" 
+or last_name like "%e";
+
+-- 7. Find all unique last names that start and end with 'E'.
+select distinct last_name
+from employees
+where last_name like "e%" 
+and last_name like "%e";
+
+-- 8. Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers.
+select *
+from employees
+where hire_date like "199%";
+
+-- 10008,10011,10012
+
+-- 9. Find all current or previous employees born on Christmas. Enter a comment with top three employee numbers.
+select *
+from employees
+where hire_date like "%-12-25";
+-- 10050,10456,10463
+
+-- 10. Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with top three employee numbers.
+select *
+from employees
+where hire_date like "199%"
+	and birth_date like "%-12-25";
+-- 10261,10438,10681
+
+-- 11. Find all unique last names that have a 'q' in their last name.
+select distinct last_name
+from employees
+where last_name like "%q%";
+
+-- 12. Find all unique last names that have a 'q' in their last name but not 'qu'.
+select distinct last_name
+from employees
+where last_name like "%q%"
+    and last_name not like "%qu%";
 
