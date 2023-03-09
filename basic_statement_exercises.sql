3d2d9e03173492f4f4a2430cb1ae6825c040ef7d
Scott Barnett <scottbarnett97@gmail.com>
2023-03-09 10:14:07 -0600

    added basic_statment_exercises.sql

---
+++ basic_statement_exercises.sql
@@ -0,0 +1,76 @@
+-- 1. Use the albums_db database.
+show databases;
+use albums_db;
+select database ();
+show tables;
+select * 
+from albums;
+
+-- 2. What is the primary key for the albums table?
+-- id
+
+-- 3. What does the column named 'name' represent?
+-- The name of a album
+
+-- 4.  What do you think the sales column represents?
+-- number of copies the album sold (in millions)
+
+-- 5. Find the name of all albums by Pink Floyd.
+/* select distinct artist, name 
+from albums
+where artist = "Pink Floyd";
+*/
+-- The Dark Side of the Moon, The Wall
+
+-- 6. What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
+/*
+select name, release_date
+from albums
+where name = "Sgt. Pepper's Lonely Hearts Club Band";
+*/
+-- 1967
+
+-- 7. What is the genre for the album Nevermind?
+/*
+select name, genre
+from albums
+where name = "Nevermind";
+*/
+-- Grunge, Alternative rock
+
+-- 8. Which albums were released in the 1990s?
+/*
+select name, release_date
+from albums
+where release_date between 1990 and 1999;
+*/
+-- The Bodyguard,Jagged Little Pill, Come On Over,  Falling into You, Let's Talk About Love, Dangerous, The Immaculate Collection,
+-- Titanic: Music from the Motion Picture, Metallica, Nevermind, Supernatural
+
+-- 9. Which albums had less than 20 million certified sales?
+/*
+select name, sales
+from albums 
+where sales < 20.0;
+*/
+-- Grease: The Original Soundtrack from the Motion Picture, Bad, Sgt. Pepper's Lonely Hearts Club Band, Dirty Dancing, Let's Talk About Love,
+-- Dangerous, The Immaculate Collection, Abbey Road, Born in the U.S.A., Brothers in Arms, Titanic: Music from the Motion Picture,
+-- Nevermind, The Wall
+
+
+SELECT name FROM albums WHERE sales < 20 ;
+
+/*
+'Grease: The Original Soundtrack from the Motion Picture'
+'Bad'
+'Sgt. Pepper\'s Lonely Hearts Club Band'
+'Dirty Dancing'
+'Let\'s Talk About Love'
+'Dangerous'
+'The Immaculate Collection'
+'Abbey Road'
+'Born in the U.S.A.'
+'Brothers in Arms'
+'Titanic: Music from the Motion Picture'
+'Nevermind'
+*/
